a2db20d66c508fd7f75c6efbdcadcddf1faa077e
Marcel Benz <marcelbenz@gmx.de>
2023-06-22 14:30:11 +0200

    second

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Minigenome Data
+#'
+#' A dataset of different Ebola virus NP-Mutants in minigenome assay
+#'
+#' @format A data frame with 17 variables:
+#' \describe{
+#' \item{\code{NP-wt}}{wildtype form of EBOV NP}
+#' \item{\code{S581A}}{the genus}
+#' \item{\code{S581D}}{the tribus}
+#' \item{\code{S587A}}{the subfamily}
+#' \item{\code{S587D}}{the range size of the species, based on the modelled distribution}
+#' \item{\code{S647A}}{the extent of occurrence}
+#' \item{\code{S647D}}{the area of occupancy}
+#' \item{\code{T563A}}{the number of independent populations deived during automated assessment}
+#' \item{\code{T563D}}{The detailled code of the conservation status based on automated assessment}
+#' \item{\code{}}{The conservation status of the species based on automated conservation assessment}
+#' \item{\code{tr_growth_form}}{the growth form of the species from the world checklist of selected plant families}
+#' \item{\code{tax_canonical}}{the canonical species name}
+#' }
+#'
+#'
+"minigenome_data"
