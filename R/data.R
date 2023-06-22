80119d5f1b015961bbec457c69ceea3b6eacee43
Marcel Benz <marcelbenz@gmx.de>
2023-06-22 14:50:34 +0200

    R data

--- R/data.R
+++ R/data.R
@@ -4,19 +4,24 @@
 #'
 #' @format A data frame with 17 variables:
 #' \describe{
-#' \item{\code{NP-wt}}{wildtype form of EBOV NP}
-#' \item{\code{S581A}}{the genus}
-#' \item{\code{S581D}}{the tribus}
-#' \item{\code{S587A}}{the subfamily}
-#' \item{\code{S587D}}{the range size of the species, based on the modelled distribution}
-#' \item{\code{S647A}}{the extent of occurrence}
-#' \item{\code{S647D}}{the area of occupancy}
-#' \item{\code{T563A}}{the number of independent populations deived during automated assessment}
-#' \item{\code{T563D}}{The detailled code of the conservation status based on automated assessment}
-#' \item{\code{}}{The conservation status of the species based on automated conservation assessment}
-#' \item{\code{tr_growth_form}}{the growth form of the species from the world checklist of selected plant families}
-#' \item{\code{tax_canonical}}{the canonical species name}
-#' }
+#' \item{\code{Experiment ID}}{Replicate of the Experiment}
+#' \item{\code{wt}}{relative Minigenom Aktivität des Wildtyp NP}
+#' \item{\code{V199A}}{relative Minigenom Aktivität der V199A NP Mutante}
+#' \item{\code{F201A}}{relative Minigenom Aktivität der F201A NP Mutante}
+#' \item{\code{V199F201AA}}{relative Minigenom Aktivität der V199A/F201A NP Mutante}
+#' \item{\code{R174A}}{relative Minigenom Aktivität der R174A NP Mutante}
+#' \item{\code{V174I176V178AAA}}{relative Minigenom Aktivität der R174A I176A V178A NP Mutante}
+#' \item{\code{V174I176V178V199F201AAAAA}}{relative Minigenom Aktivität der R174A I176A V178A V199A/F201A NP Mutante}
+#' \item{\code{S581A}}{relative Minigenom Aktivität der S581A NP Mutante}
+#' \item{\code{S581D}}{relative Minigenom Aktivität der S581D NP Mutante}
+#' \item{\code{S587A}}{relative Minigenom Aktivität der S587A NP Mutante}
+#' \item{\code{S587D}}{relative Minigenom Aktivität der S587D NP Mutante}
+#' \item{\code{S647A}}{relative Minigenom Aktivität der S647A NP Mutante}
+#' \item{\code{S647D}}{relative Minigenom Aktivität der S647D NP Mutante}
+#' \item{\code{T563A}}{relative Minigenom Aktivität der T563A NP Mutante}
+#' \item{\code{T563D}}{relative Minigenom Aktivität der T563D NP Mutante}
+#' \item{\code{ohneNP}}{relative Minigenom Aktivität der ohne NP Mutante}
+
 #'
 #'
 "minigenome_data"
