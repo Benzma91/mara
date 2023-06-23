1e06c23b5f5214b71142faee2902d7ab0e5180b8
Marcel Benz <marcelbenz@gmx.de>
2023-06-23 11:36:00 +0200

    Readme

--- R/MGPbar.R
+++ R/MGPbar.R
@@ -19,12 +19,15 @@ MGPbar <- function(x){
   
   dat_long <- pivot_longer(x,
                            WT:ohneNP,
-                           values_tMGo = "prop_activity",
+                           values_to = "prop_activity",
                            names_to = "mutant") %>% 
     mutate(ID = as.factor(ID))
   
   ggplot()+
-    geom_bar(data = dat_long, aes(x = prop_activity, y = mutant, color = ID))+
+    geom_bar(data = dat_long, 
+             aes(y = mutant, x=prop_activity, fill = ID),
+             position = "dodge",
+             stat = "identity")+
     scale_color_discrete()
   
 }
