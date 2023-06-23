b1fecf586faf0e3784b4eeb1f106b6a93fb4d78c
Marcel Benz <marcelbenz@gmx.de>
2023-06-23 10:30:01 +0200

    2nd Day, function without errors =)

--- R/MGPbar.R
+++ R/MGPbar.R
@@ -14,16 +14,17 @@
 #' @export
 
 
-MGP <- function(x){
+
+MGPbar <- function(x){
   
   dat_long <- pivot_longer(x,
                            WT:ohneNP,
-                           values_to = "prop_activity",
+                           values_tMGo = "prop_activity",
                            names_to = "mutant") %>% 
     mutate(ID = as.factor(ID))
   
   ggplot()+
-    geom_point(data = dat_long, aes(x = prop_activity, y = mutant, color = ID))+
+    geom_bar(data = dat_long, aes(x = prop_activity, y = mutant, color = ID))+
     scale_color_discrete()
-
+  
 }
--- R/data.R
+++ R/data.R
@@ -4,24 +4,21 @@
 #'
 #' @format A data frame with 17 variables:
 #' \describe{
-#' \item{\code{Experiment ID}}{Replicate of the Experiment}
-#' \item{\code{wt}}{relative Minigenom Aktivität des Wildtyp NP}
+#' \item{\code{ID}}{Replicate of the Experiment}
+#' \item{\code{WT}}{relative Minigenom Aktivität des Wildtyp NP}
 #' \item{\code{V199A}}{relative Minigenom Aktivität der V199A NP Mutante}
 #' \item{\code{F201A}}{relative Minigenom Aktivität der F201A NP Mutante}
 #' \item{\code{V199F201AA}}{relative Minigenom Aktivität der V199A/F201A NP Mutante}
 #' \item{\code{R174A}}{relative Minigenom Aktivität der R174A NP Mutante}
-#' \item{\code{V174I176V178AAA}}{relative Minigenom Aktivität der R174A I176A V178A NP Mutante}
-#' \item{\code{V174I176V178V199F201AAAAA}}{relative Minigenom Aktivität der R174A I176A V178A V199A/F201A NP Mutante}
+#' \item{\code{R174I176V178AAA}}{relative Minigenom Aktivität der R174A I176A V178A NP Mutante}
+#' \item{\code{R174I176V178V199F201AAAAA}}{relative Minigenom Aktivität der R174A I176A V178A V199A/F201A NP Mutante}
 #' \item{\code{S581A}}{relative Minigenom Aktivität der S581A NP Mutante}
 #' \item{\code{S581D}}{relative Minigenom Aktivität der S581D NP Mutante}
 #' \item{\code{S587A}}{relative Minigenom Aktivität der S587A NP Mutante}
 #' \item{\code{S587D}}{relative Minigenom Aktivität der S587D NP Mutante}
 #' \item{\code{S647A}}{relative Minigenom Aktivität der S647A NP Mutante}
 #' \item{\code{S647D}}{relative Minigenom Aktivität der S647D NP Mutante}
-#' \item{\code{T563A}}{relative Minigenom Aktivität der T563A NP Mutante}
-#' \item{\code{T563D}}{relative Minigenom Aktivität der T563D NP Mutante}
 #' \item{\code{ohneNP}}{relative Minigenom Aktivität der ohne NP Mutante}
-
+#'}
 #'
-#'
-"minigenome_data"
+"Mara_Minigenom"
